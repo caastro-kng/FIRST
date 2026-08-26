a607785bef978a909a77c725b956ec96d228d381
caastro-kng <castrolendario@gmail.com>
2026-08-26 10:09:41 -0300

    build: centralize presentation image manifest

---
+++ scripts/image-manifest.mjs
@@ -0,0 +1,29 @@
+export const SITE_IMAGES = [
+  { file: 'hero-championship.jpg', source: 'https://community.firstinspires.org/hubfs/first-blog_community_cmpupdates2-024.jpg' },
+
+  { file: 'league-fll.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2026/06_junho/3-6-2026__Sesi_GAMA_Torneio_Interno_de_Rob%C3%B3tica_FLLC_Foto_Victor_Hugo_Pessoa_CAPA-16.jpg' },
+  { file: 'league-ftc.jpg', source: 'https://community.firstinspires.org/hubfs/preview%20event.jpg' },
+  { file: 'league-frc.jpg', source: 'https://community.firstinspires.org/hubfs/blog/frc/2025-robot-rules-preview-for-2026.jpg' },
+
+  { file: 'journey-01.jpg', source: 'https://www.firstinspires.org/hs-fs/hubfs/20230420_bm_0312.jpg?height=1260&name=20230420_bm_0312.jpg&width=1890' },
+  { file: 'journey-02.webp', source: 'https://www.firstinspires.org/hs-fs/hubfs/20230420_bm_0446_1.webp?name=20230420_bm_0446_1.webp&width=2000' },
+  { file: 'journey-03.webp', source: 'https://www.firstinspires.org/hs-fs/hubfs/frc_getstarted_1260hero.webp?height=840&name=frc_getstarted_1260hero.webp&width=1260' },
+  { file: 'journey-04.png', source: 'https://community.firstinspires.org/hs-fs/hubfs/undefined-Mar-09-2026-05-09-00-4625-PM.png?height=830&name=undefined-Mar-09-2026-05-09-00-4625-PM.png&width=638' },
+
+  { file: 'season-01.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2024/Fevereiro/EQUIPE_FRC_-_Robots_District_-_Taguatinga_-_Foto_Bruno_Frauzino_corpo.jpg' },
+  { file: 'season-02.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2026/06_junho/3-6-2026__Sesi_GAMA_Torneio_Interno_de_Rob%C3%B3tica_FLLC_Foto_Victor_Hugo_Pessoa_CAPA-31.jpg' },
+  { file: 'season-03.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2024/Fevereiro/EQUIPE_FRC_-_Robots_District_-_Taguatinga_-_Foto_Bruno_Frauzino_2.jpg' },
+  { file: 'season-04.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2024/Fevereiro/EQUIPE_FRC_-_Robots_District_-_Taguatinga_-_Foto_Bruno_Frauzino_1.jpg' },
+  { file: 'season-05.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2024/Fevereiro/EQUIPE_FRC_-_Robots_District_-_Taguatinga_-_Foto_Bruno_Frauzino_3.jpg' },
+  { file: 'season-06.jpg', source: 'https://www.sistemafibra.org.br/senai/images/categorias/noticias/2026/04-abril/29-04-2026_Equipe_Robots_District_Foto_Dayane_dos_Santos_Sistema_Fibra.jpg' },
+  { file: 'season-07.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2025/04_abril/16-04-FRC-Samira-Padua-Sistema-Fibra-1.jpg' },
+
+  { file: 'competition-arena.webp', source: 'https://www.firstinspires.org/hs-fs/hubfs/image-library/web/20250417dan1701-1260x840.webp?height=840&name=20250417dan1701-1260x840.webp&width=1260' },
+  { file: 'competition-pits.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2025/04_abril/16-04-FRC-Samira-Padua-Sistema-Fibra-2.jpg' },
+
+  { file: 'brazil-9484.jpg', source: 'https://www.sistemafibra.org.br/senai/images/categorias/noticias/2026/02-fevereiro/23-1-2026_Equipes_FRC_-_Foto_Bruno_Frauzino-2827_ROBOTS_DISTRICT.jpg' },
+  { file: 'brazil-houston.jpg', source: 'https://www.sistemafibra.org.br/senai/images/categorias/noticias/2026/05-maio/RobotsDistrict-no-First-Championship-2026-FotoDayanedosSantos-SistemaFibra-1.5.jpg' },
+  { file: 'brazil-federal-force.jpg', source: 'https://www.sistemafibra.org.br/sesi/images/categorias/noticias/2025/04_abril/interna-Foto-SamiraPadua-SistemaFibra1.jpg' },
+];
+
+export const localImagePath = (file) => `/images/site/${file}`;
